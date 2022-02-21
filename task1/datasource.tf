ddbe7afde3ef48bdc857fc63b75b79b071a02ea3
Sergey Chulanov <tty8747@gmail.com>
2022-02-21 09:48:10 +0300

    Added solution for task01

---
+++ task1/datasource.tf
@@ -0,0 +1,24 @@
+data "aws_vpcs" "listVPCs" {
+}
+
+data "aws_subnets" "listSubnets" {
+  filter {
+    name   = "vpc-id"
+    values = [for i in data.aws_vpcs.listVPCs.ids : i]
+  }
+}
+
+output "subnets" {
+  value = [for i in data.aws_subnets.listSubnets.ids : i]
+}
+
+data "aws_security_groups" "listSG" {
+  filter {
+    name   = "vpc-id"
+    values = [for i in data.aws_vpcs.listVPCs.ids : i]
+  }
+}
+
+output "sgroups" {
+  value = [for i in data.aws_security_groups.listSG.ids : i]
+}
